c29255317e46c398fa7dc1a5c07d2edbc7acc377
Vineeth Raj <contact.twn@opmbx.org>
2014-06-21 16:50:43 +0530

    add uksm 0.1.2.2 for v3.0 .ge.84.patch
    Change-Id: I623db724c95aadb084252cb769c7dc04868c1f64

--- include/linux/input/sdt2wake.h
+++ include/linux/input/sdt2wake.h
@@ -1,6 +1,6 @@
-#ifndef _LINUX_SDT2WAKE_H
-#define _LINUX_SDT2WAKE_H
+#ifndef _LINUX_2WAKE_H
+#define _LINUX_2WAKE_H
 
 extern bool is_screen_on;
 
-#endif	/* _LINUX_SDT2WAKE_H */
+#endif	/* _LINUX_2WAKE_H */
