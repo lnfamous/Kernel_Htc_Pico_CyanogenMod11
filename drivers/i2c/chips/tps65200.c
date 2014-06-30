017cf3954e459a7de1fcd26385c6688abdfd2b34
Sultanxda <sultanxda@gmail.com>
2014-06-30 12:50:10 -0700

    i2c: tps65200: Use fast charge rate when slow charge is requested
    For AC-charge rate over USB.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/i2c/chips/tps65200.c
+++ drivers/i2c/chips/tps65200.c
@@ -416,7 +416,6 @@ int tps_set_charger_ctrl(u32 ctl)
 		/* cancel CHECK_CHG alarm */
 		alarm_cancel(&tps65200_check_alarm);
 		break;
-	case POWER_SUPPLY_ENABLE_SLOW_CHARGE:
 	case POWER_SUPPLY_ENABLE_WIRELESS_CHARGE:
 		tps65200_dump_register();
 		tps65200_i2c_write_byte(0x29, 0x01);
@@ -456,6 +455,7 @@ int tps_set_charger_ctrl(u32 ctl)
 		}
 #endif /* SET_VDPM_AS_476 */
 		break;
+	case POWER_SUPPLY_ENABLE_SLOW_CHARGE:
 	case POWER_SUPPLY_ENABLE_FAST_CHARGE:
 		tps65200_dump_register();
 		tps65200_i2c_write_byte(0x29, 0x01);
