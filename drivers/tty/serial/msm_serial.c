f5bfc0390eb0f1f3007cfc66b813a18f74b1b0b9
Vineeth Raj <contact.twn@opmbx.org>
2014-07-01 16:01:09 +0530

    drivers/tty: Add back htc related stuff
    Add back files drivers/tty/serial/msm_serial_hs_brcm.c
    && drivers/tty/serial/msm_serial_hs_ti_dc.c which fixes
    bluetooth and add back some of the CONFIG_MSM_SMD0_WQ
    HTC hax.
    
    Change-Id: I47b3a65f6e2148411661aeac68ba42df3b5cf8e7

--- drivers/tty/serial/msm_serial.c
+++ drivers/tty/serial/msm_serial.c
@@ -36,6 +36,7 @@
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <mach/msm_serial_pdata.h>
+#include <mach/board_htc.h>
 #include "msm_serial.h"
 
 
@@ -74,6 +75,9 @@ struct msm_port {
 	struct msm_wakeup wakeup;
 #endif
 };
+
+
+static int msm_serial_enable;
 
 #define UART_TO_MSM(uart_port)	((struct msm_port *) uart_port)
 #define is_console(port)	((port)->cons && \
@@ -570,6 +574,10 @@ static int msm_startup(struct uart_port *port)
 	if (unlikely(ret))
 		return ret;
 
+	if (unlikely(irq_set_irq_wake(port->irq, 1))) {
+		free_irq(port->irq, port);
+		return -ENXIO;
+	}
 
 #ifndef CONFIG_PM_RUNTIME
 	msm_init_clock(port);
@@ -1155,6 +1163,13 @@ static int __init msm_serial_init(void)
 {
 	int ret;
 
+		/* Switch Uart Debug by Kernel Flag  */
+	if (get_kernel_flag() & KERNEL_FLAG_SERIAL_HSL_ENABLE)
+		msm_serial_enable = 1;
+
+	if (!msm_serial_enable)
+		msm_uart_driver.cons = NULL;
+
 	ret = uart_register_driver(&msm_uart_driver);
 	if (unlikely(ret))
 		return ret;
@@ -1171,7 +1186,8 @@ static int __init msm_serial_init(void)
 static void __exit msm_serial_exit(void)
 {
 #ifdef CONFIG_SERIAL_MSM_CONSOLE
-	unregister_console(&msm_console);
+	if (msm_serial_enable)
+		unregister_console(&msm_console);
 #endif
 	platform_driver_unregister(&msm_platform_driver);
 	uart_unregister_driver(&msm_uart_driver);
