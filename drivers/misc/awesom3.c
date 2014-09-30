66e8a7933115160a38f22304e735dbd3f759ef84
Vineeth Raj <contact.twn@openmailbox.org>
2014-09-30 23:58:11 +0530

    awesom3: allow a 128 mB userdata

--- drivers/misc/awesom3.c
+++ drivers/misc/awesom3.c
@@ -58,7 +58,7 @@
 #define CACHE_SIZE_LEAVE 8
 
 // no. of mB's to be left in the 'userdata' partition
-#define USERDATA_SIZE_LEAVE 4
+#define USERDATA_SIZE_LEAVE 128
 
 // no. of mB's to be left in the 'devlog' partition
 #define DEVLOG_SIZE_LEAVE 1
